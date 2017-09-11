6b5b9c35a2821d9419b9daccd06c75149afbcbad
Uri Jonathan Bravo Mendez <urbix.traceur@outlook.com>
2017-09-10 23:30:30 -0500

    Agregado metodo que convierte el grafo a matriz

--- Grafo/GrafoObject.cs
+++ Grafo/GrafoObject.cs
@@ -11,12 +11,15 @@ namespace Grafo
         public List<Vertice> VerticesL { get; set; }
         public List<Arista> AristasL { get; set; }
 
+        public const int INF = 99999;
+
         public GrafoObject()
         {
             VerticesL = new List<Vertice>();
             AristasL = new List<Arista>();
         }
 
+        //Crea una arista entre dos vertices dados
         public void AgregarConexion(Vertice verticeInicial, Vertice verticeFinal, int peso)
         {
             //se crea una nueva arista con los parametros del metodo
@@ -34,9 +37,8 @@ namespace Grafo
             AristasL.Add(arista);
         }
 
-
         //Permite atravez de una expresion lamda saber si existe o no el vertice
-        public bool ExisteVertice(Vertice aBuscar)
+        private bool ExisteVertice(Vertice aBuscar)
         {
             var it = VerticesL.Where(x => x.Nombre == aBuscar.Nombre).FirstOrDefault();
             if (it == null)
@@ -44,5 +46,48 @@ namespace Grafo
             return true;             
         }
 
+        public int[,] ConvertirListaVerticesAArray()
+        {
+            int[,] matriz = new int[VerticesL.Count, VerticesL.Count];
+            byte c = 0;
+
+            for (int i = 0; i<VerticesL.Count; i++)
+            {
+                for (int j = 0; j< VerticesL.Count; j++)
+                {
+                    matriz[i, j] = INF;
+                }
+            }
+            
+            foreach (var vertice in VerticesL)
+            {
+                foreach (var arista in AristasL)
+                {
+                    bool entro = false;
+                    if (arista.Vertices.vInicial.Nombre.Equals(vertice.Nombre))
+                    {
+                        for (int i = 0; i<VerticesL.Count; i++)
+                        {
+                            if (arista.Vertices.vFinal.Nombre.Equals(VerticesL[i].Nombre))
+                            {
+                                matriz[c, i] = arista.Peso;
+                                matriz[i, c] = arista.Peso;
+                            }
+                            
+                        }
+                    }
+                }
+                c++;
+            }
+
+
+            for (int t = 0; t < VerticesL.Count; t++)
+            {
+                matriz[t, t] = 0;
+            }
+
+            return matriz;
+        }
+        
     }
 }
--- Grafo/Program.cs
+++ Grafo/Program.cs
@@ -27,10 +27,31 @@ namespace Grafo
             g.AgregarConexion(b, d, 7);
             g.AgregarConexion(d, c, 3);
 
+            foreach (var item in g.VerticesL)
+            {
+                Console.WriteLine($"{item.Nombre} \n");
+            }
+
             foreach (var item in g.AristasL)
             {
-                Console.WriteLine($"{item.ToString()} \n");
+                Console.WriteLine(item.ToString());
             }
+
+            Console.WriteLine("\n\n");
+
+            var matriz  = g.ConvertirListaVerticesAArray();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < g.VerticesL.Count; i++)
+            {
+                sb.Append(g.VerticesL[i].Nombre + "    ");
+                for (int j = 0; j < g.VerticesL.Count; j++)
+                {
+                    sb.Append(matriz[i, j] + " ");
+                }
+
+                sb.Append("\n");
+            }
+            Console.WriteLine(sb.ToString());
             Console.ReadKey();
         }
     }
